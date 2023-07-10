44565d797f49ce03388f18a8865b41370ce89b81
suyc17 <judy102017@gmail.com>
2023-07-10 21:09:15 +0800

    test

---
+++ frontend/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <input type="text">
+    </div>
+</template>
+
+<style>
+input {
+    border-radius: 5px;
+}
+</style>
+
+<script>
+fetch('https://google.com/')
+.then((x) => x.text())
+.then((x) => console.log(x));
+</script>
